632eb2bc991e65612af34a8d92da0975439d8957
Devin <ospreywebservices@gmail.com>
2021-04-08 17:47:16 -0400

    finished player versus player functionality

--- javaScript.js
+++ javaScript.js
@@ -14,6 +14,7 @@ const boardDisplay = document.getElementById('boardDisplay');
 const difficulty = document.getElementById('difficulty');
 const gameBlocks = document.querySelectorAll('[data-block]');
 
+//player versus player activation
 submitButton.addEventListener('click', () => {
     const Gameboard = (() => {
         let play = true;
@@ -33,6 +34,7 @@ submitButton.addEventListener('click', () => {
         const player1 = Player(playerOne, 'O', true);
         const player2 = Player(playerTwo, 'X', false);
         displayController.removeMain();
+        displayController.playerDisplay(player1.name, player2.name);
         console.log({ player1, player2 });
 
         //rotate player 1 and player 2 turns, push into array and compare winning combos
@@ -73,7 +75,6 @@ submitButton.addEventListener('click', () => {
                             }
                         console.log(indexesX);
                         checkWinner(indexesX, player2.name);
-                        console.log(play)
                     }
                 })
             })
@@ -94,23 +95,59 @@ submitButton.addEventListener('click', () => {
             winCombos.forEach(combo => {
                 const check = (indexes, combo) => combo.every(v => indexes.includes(v));
                 if (check(indexes, combo) === true) {
-                    console.log('winner!', player);
                     displayController.winnerScreen(player);
+                    play = displayController.play;
                 }
             })
         };
     })();
 });
 
+//player versus computer activation;
+/*
+submitButton2.addEventListener('click', () => {
+    let play = true;
+    let board = [];
+    if (play === true) {
+        gameBlocks.forEach(gameBlocks => {
+            board.push("");
+        })
+    }
+
+    const computerGameboard = (() => {
+        const PVC = (name, letter, turn) => {
+            return { name, letter, turn };
+        };
+
+        let username = playerVersusComputerName.value;
+        let difficultyChoice = difficulty.value;
+        if (username === "") {
+            username = 'Player 1';
+        }
+        const player = PVC(username, "O", true);
+        const computer = PVC("Computer", 'X', false);
+        console.log(player, computer, difficultyChoice);
+        displayController.removeMain();
+        if (difficultyChoice === "easy") {
+            easy(player, computer);
+        } else if (difficultyChoice === "normal") {
+            normal(player, computer);
+        } else if (difficultyChoice === "impossible") {
+            impossible(player, computer)
+        }
+        //checkWinner()
+    })();
+});
+*/
+
 const displayController = (() => {
     let play = false;
     let playerContainer = document.getElementById('playerContainer');
     let restart = document.getElementById('restart');
     let resetContainer = document.getElementById('resetContainer');
-    let p1 = document.getElementById('p1');
-    let p2 = document.getElementById('p2');
 
-   const removeMain = () => {
+
+    const removeMain = () => {
         userMain.remove();
         playerInfo.remove();
         computerInfo.remove();
@@ -121,9 +158,16 @@ const displayController = (() => {
         resetContainer.style.display = 'flex';
     };
 
+    const playerDisplay = (player1, player2) => {
+        let p1 = document.getElementById('p1');
+        let p2 = document.getElementById('p2');
+        p1.textContent = `Player 1: ${player1} - "O"`;
+        p2.textContent = `Player 2: ${player2} - "X"`;
+    };
+
     const winnerScreen = (player) => {
-        console.log('winnerscreen')
-        console.log(player);
+        let winner = document.getElementById('winner');
+        winner.textContent = `${player} is the winner!`;
     }
 
 
@@ -148,22 +192,50 @@ const displayController = (() => {
     });
 
     restart.addEventListener('click', () => {
-
+        window.location.reload();
     });
-    return { winnerScreen, removeMain, play };
+
+    return { winnerScreen, removeMain, playerDisplay, play };
 })();
 
-/*
+/*const difficultyFunctions = (() => {
+    const easy = (player, computer) => {
+        gameBlocks.forEach(block => {
+            block.addEventListener('click', (e) => {
+                if (e.target.textContent === 'O' || e.target.textContent === 'X' || play === false) {
+                    return;
+                }
+                if (player1.turn === true && computer.turn === false) {
+                    e.target.textContent = `${player1.letter}`;
+                    player1.turn = false;
+                    player2.turn = true;
+                    board.splice(e.target.id, 1, player1.letter);
+                    console.log(board)
+                    let indexesO = [], i;
+                    for (i = 0; i < board.length; i++)
+                        if (board[i] === "O") {
+                            indexesO.push(i);
+                        }
+                    console.log(indexesO);
+                    checkWinner(indexesO, player1.name);
 
-submitButton2.addEventListener('click', () => {
-       let userName = playerVersusComputerName.value;
-       let difficultyChoice = difficulty.value;
-       if (userName === "") {
-           userName = 'Player 1';
-       }
-       removeMain();
-       return (user1 = PVC(userName, 'O', 'X', difficultyChoice));
-   });
-   const PVC = (userName, userLetter, computerLetter, difficultyChoice) => {
-       return { userName, userLetter, computerLetter, difficultyChoice };
-   };*/
+                } else if (player2.turn === true && player1.turn === false) {
+                    e.target.textContent = `${player2.letter}`;
+                    player1.turn = true;
+                    player2.turn = false;
+                    board.splice(e.target.id, 1, player2.letter);
+                    console.log(board);
+                    let indexesX = [], i;
+                    for (i = 0; i < board.length; i++)
+                        if (board[i] === "X") {
+                            indexesX.push(i);
+                        }
+                    console.log(indexesX);
+                    checkWinner(indexesX, player2.name);
+                }
+            })
+        })
+    }
+    return { easy, normal, impossible };
+})();
+*/
